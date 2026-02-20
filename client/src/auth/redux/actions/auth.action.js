d99fe548c87f2d38e6d7e5bab5e624ca7ab83b2f
abhinandanchivate <abhinandanchivate2000@gmail.com>
2026-02-20 10:59:38 +0530

    Add registerUserAction with async handling in Redux slice and actions

---
+++ client/src/auth/redux/actions/auth.action.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { registerUser } from "../../service/auth.service";
+
+export const registerUserAction = createAsyncThunk(
+  "auth/registerUserAction", // action name, it should be unique to ur application. ---> we will use the name of the reducer followed by / and then action name
+  async (formData, { rejectWithValue }) => {
+    try {
+      const data = await registerUser(formData);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.data);
+    }
+  },
+  // async : to perform asynchronous operations like api calls to the backend
+  // formData : argument which we will pass from the component when we will dispatch this action to perform the api call to the backend for registering the user.
+  // rejectWithValue : it is a function which is provided by the createAsyncThunk to handle the error response from the backend. --> it will help us to return the error response from the backend to the component where we are dispatching this action to perform some actions based on the error response from the backend.
+);
+// createAsyncThunk:
+// create: to create the action
+// async: to perform asynchronous operations like api calls to the backend
+// thunk(middleware part ): it will help us to dispatch multiple actions based on the response from the backend. --> pending, fulfilled, rejected actions will be dispatched based on the response from the backend. --> we can perform some actions in the component based on these actions which are dispatched by the thunk. --> we can show loading spinner when the pending action is dispatched, we can show success message when the fulfilled action is dispatched and we can show error message when the rejected action is dispatched. --> it will help us to manage the state of our application based on the response from the backend.
+// middleware now it is managed by redux toolkit
+// pending/ inprogress : when the api call is in progress
+
+// fulfilled/ success : when the api call is successful
+// rejected/failure : when the api call is failed
--- client/src/auth/redux/slices/index.js
+++ client/src/auth/redux/slices/index.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { registerUserAction } from "../actions/auth.action";
 const authState = {
   isAuthenticated: false,
   user: null,
@@ -12,6 +13,34 @@ const authSlice = createSlice({
   name: "auth",
   initialState: authState,
   reducers: {},
+  // reducers: to handle only sync actions where we can have direct manipulation of the state
+  // logout : isAuthenticated : false --> direct manipulation fo the state.
+
+  extraReducers: (builder) => {
+    // u will come across only 3 action based situations : no more extra actions are allowed here.
+
+    // pending action
+    builder.addCase(registerUserAction.pending, (state) => {});
+    // fulfilled action/ success action
+    builder.addCase(registerUserAction.fulfilled, (state, action) => {
+      state.isAuthenticated = true;
+      state.user = action.payload.data.user;
+      state.token = action.payload.data.token;
+      state.status = action.payload.status;
+      state.loading = false;
+      state.error = null;
+    });
+    // rejected action/ failure action
+    builder.addCase(registerUserAction.rejected, (state, action) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.token = null;
+      state.status = action.payload.status;
+      state.loading = false;
+      state.error = action.payload.data.message;
+    });
+  },
+  // extrareducers : used to handle the rest based actions
 });
 
 // slice is helping us to get the reducers
